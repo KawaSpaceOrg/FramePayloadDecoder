575f9bf1a646eccf31341d058e1482ff2b28b313
Meghashyam Chirravoori <c.meghashyam@gmail.com>
2021-01-18 18:30:53 +0530

    added field _current_time

--- example.py
+++ example.py
@@ -2,11 +2,15 @@ from payload_decoder import PayloadDecoder
 import pprint
 
 
-raw_frame_payload = open('example_frames/telemetry_frame_payload.bin', 'rb').read()
+
+raw_frame_payload = open('example_frames/example.bin', 'rb').read()
 pprint.pprint(PayloadDecoder.decode(raw_frame_payload))
 
-raw_frame_payload = open('example_frames/periodic_frame_payload.bin', 'rb').read()
-pprint.pprint(PayloadDecoder.decode(raw_frame_payload))
+# raw_frame_payload = open('example_frames/telemetry_frame_payload.bin', 'rb').read()
+# pprint.pprint(PayloadDecoder.decode(raw_frame_payload))
 
-raw_frame_payload = open('example_frames/file_list_frame_payload.bin', 'rb').read()
-pprint.pprint(PayloadDecoder.decode(raw_frame_payload))
+# raw_frame_payload = open('example_frames/periodic_frame_payload.bin', 'rb').read()
+# pprint.pprint(PayloadDecoder.decode(raw_frame_payload))
+
+# raw_frame_payload = open('example_frames/file_list_frame_payload.bin', 'rb').read()
+# pprint.pprint(PayloadDecoder.decode(raw_frame_payload))
