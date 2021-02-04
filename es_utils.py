bdb34d81954559ceefdc18d07eed891c2930ba37
Meghashyam Chirravoori <c.meghashyam@gmail.com>
2021-02-04 14:35:56 +0530

    changed elastic search env variable

--- es_utils.py
+++ es_utils.py
@@ -1,11 +1,9 @@
 import datetime, os
 from elasticsearch import Elasticsearch
 
-es_hosts =  [os.getenv('ES_HOST1'),os.getenv('ES_HOST2'),os.getenv('ES_HOST3')]
-
 es = Elasticsearch(
-    es_hosts,
-    http_auth=(os.getenv('ES_USER'), os.getenv('ES_PASSWORD1')),
+    [os.getenv('ES_HOST')],
+    http_auth=(os.getenv('ES_USER'), os.getenv('ES_PASSWORD')),
     sniff_on_start=True
 )
 
